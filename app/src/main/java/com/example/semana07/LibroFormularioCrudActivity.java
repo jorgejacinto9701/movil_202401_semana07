334aa061538d43d64149a85967bf08f911d25748
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-06-15 08:24:23 -0500

    carga de combos

--- app/src/main/java/com/example/semana07/LibroFormularioCrudActivity.java
+++ app/src/main/java/com/example/semana07/LibroFormularioCrudActivity.java
@@ -15,10 +15,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.semana07.entity.Categoria;
 import com.example.semana07.entity.Libro;
+import com.example.semana07.entity.Pais;
+import com.example.semana07.service.ServiceCategoria;
 import com.example.semana07.service.ServiceLibro;
+import com.example.semana07.service.ServicePais;
+import com.example.semana07.util.ConnectionRest;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LibroFormularioCrudActivity extends AppCompatActivity {
 
@@ -34,8 +44,8 @@ public class LibroFormularioCrudActivity extends AppCompatActivity {
 
     //Servicio
     ServiceLibro serviceLibro;
-    //ServicePais servicePais;
-    //ServiceCategoriaLibro serviceCategoriaLibro;
+    ServicePais servicePais;
+    ServiceCategoria   serviceCategoria;
 
     EditText txtTitulo, txtAnio, txtSerie;
 
@@ -46,6 +56,7 @@ public class LibroFormularioCrudActivity extends AppCompatActivity {
     String metodo;
 
     Libro objActual;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +69,18 @@ public class LibroFormularioCrudActivity extends AppCompatActivity {
             return insets;
         });
 
+
+        servicePais = ConnectionRest.getConnection().create(ServicePais.class);
+        serviceCategoria = ConnectionRest.getConnection().create(ServiceCategoria.class);
+        serviceLibro = ConnectionRest.getConnection().create(ServiceLibro.class);
+
+        adaptadorPais = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, paises);
+        spnPais = findViewById(R.id.spnRegLibPais);
+        spnPais.setAdapter(adaptadorPais);
+
+        adaptadorCategoria = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, categorias);
+        spnCategoria = findViewById(R.id.spnRegLibCategoria);
+        spnCategoria.setAdapter(adaptadorCategoria);
 
         txtTitulo = findViewById(R.id.txtRegLibTitulo);
         txtAnio = findViewById(R.id.txtRegLibAnio);
@@ -92,5 +115,48 @@ public class LibroFormularioCrudActivity extends AppCompatActivity {
                     startActivity(intent);
             }
         });
+
+        cargaPais();
+        cargaCategoria();
     }
+
+    void cargaPais(){
+        Call<List<Pais>>  call = servicePais.listaTodos();
+        call.enqueue(new Callback<List<Pais>>() {
+            @Override
+            public void onResponse(Call<List<Pais>> call, Response<List<Pais>> response) {
+                    List<Pais> lstAux =  response.body();
+                    paises.add(" [ Seleccione ] ");
+                    for(Pais aux:lstAux){
+                        paises.add(aux.getIdPais() + " : "  + aux.getNombre());
+                    }
+                    adaptadorPais.notifyDataSetChanged();
+            }
+            @Override
+            public void onFailure(Call<List<Pais>> call, Throwable t) {
+
+            }
+        });
+    }
+    void cargaCategoria(){
+        Call<List<Categoria>>  call = serviceCategoria.listaTodos();
+        call.enqueue(new Callback<List<Categoria>>() {
+            @Override
+            public void onResponse(Call<List<Categoria>> call, Response<List<Categoria>> response) {
+                List<Categoria> lstAux =  response.body();
+                categorias.add(" [ Seleccione ] ");
+                for(Categoria aux:lstAux){
+                    categorias.add(aux.getIdCategoria() + " : "  + aux.getDescripcion());
+                }
+                adaptadorCategoria.notifyDataSetChanged();
+            }
+            @Override
+            public void onFailure(Call<List<Categoria>> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+
 }
